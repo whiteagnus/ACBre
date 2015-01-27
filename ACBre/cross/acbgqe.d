66f63a29968416634e050de13922ed3a9b8e02d5
whiteagnus <whiteagnus@gmx.de>
2015-01-27 21:44:37 +0100

    Complete Recode for Compatibility
    - Splitted Dialogue and Script Files for both BG Parts
    - Splitted Areas for both Parts
    - Recoded tp2 for Compatibility

--- ACBre/cross/acbgqe.d
+++ ACBre/cross/acbgqe.d
@@ -1,64 +1,64 @@
 INTERJECT_COPY_TRANS2 C#Q01001 4 ACC#Q010014
 == ACBreJ IF ~InParty("ACBre")InMyArea("ACBRE")!StateCheck("ACBre",CD_STATE_NOTVALID)~ THEN
-~Pfui Deibel! Ich bin in so manchem Stollen herumgekrochen und habe manche Widerlichkeit gesehen, <CHARNAME>. Aber *das* setzt allem die Krone auf.~
+@0
 END
 
 INTERJECT_COPY_TRANS2 C#Q01001 42 ACC#Q0100141
 == ACBreJ IF ~InParty("ACBre")See("ACBRE")!StateCheck("ACBre",CD_STATE_NOTVALID)~ THEN
-~Und starrt Löcher in die Luft, wie es nur ein närrischer <PRO_RACE> kann. Pah.~
+@1
 END
 
 INTERJECT_COPY_TRANS2 C#Q01003 1 ACC#Q010031
 == ACBreJ IF ~InParty("ACBre")See("ACBRE")!StateCheck("ACBre",CD_STATE_NOTVALID)~ THEN
-~Ihr seid für das Abenteurerleben wie geschaffen, <CHARNAME>. Keine Skrupel, in jedem Dreck herumzuwühlen.~
+@2
 END
 
 INTERJECT C#Q01004 6 ACC#Q010046
 ==ACBREJ IF ~InParty("ACBRE")See("ACBRE")!StateCheck("ACBRE",CD_STATE_NOTVALID)~ THEN
-~Jetzt hab ich aber die Nase voll! Raus mit der Sprache, oder ich schneide Euch Eure Antwort mit der Axt aus den Rippen!~
+@3
 END
 IF ~~ THEN EXTERN C#Q01004 8
 
 INTERJECT_COPY_TRANS2 C#Q03009 5 ACC#Q030095
 ==ACBreJ IF ~InParty("ACBre")See("ACBRE")!StateCheck("ACBre",CD_STATE_NOTVALID)~ THEN
-~Ja, das ist er wohl... und er krabbelt auf mich zu.~ = ~Uhm... <CHARNAME>? Hilfe?~
+@4 = @5
 ==C#Q03010 IF ~InParty("ACBre")See("ACBRE")!StateCheck("ACBre",CD_STATE_NOTVALID)~ THEN
-~Bart!~
+@6
 ==ACBreJ IF ~InParty("ACBre")See("ACBRE")!StateCheck("ACBre",CD_STATE_NOTVALID)~ THEN
-~Aua! Das ist *mein* Bart! Nicht ziehen! Nicht! Aua!~
+@7
 ==C#Q03010 IF ~InParty("ACBre")See("ACBRE")!StateCheck("ACBre",CD_STATE_NOTVALID)~ THEN
-~*kräh*~
+@8
 ==ACBreJ IF ~InParty("ACBre")See("ACBRE")!StateCheck("ACBre",CD_STATE_NOTVALID)~ THEN
-~Bei der Liebe Berronars, jetzt ist *Schluss*!~
+@9
 ==C#Q03010 IF ~InParty("ACBre")See("ACBRE")!StateCheck("ACBre",CD_STATE_NOTVALID)~ THEN
-~Naaaah! *brüll*~
+@10
 ==C#Q03009 IF ~InParty("ACBre")See("ACBRE")!StateCheck("ACBre",CD_STATE_NOTVALID)~ THEN
-~Warum bist du so klein?~
+@11
 ==ACBreJ IF ~InParty("ACBre")See("ACBRE")!StateCheck("ACBre",CD_STATE_NOTVALID)~ THEN
-~Klein? Ich bin nicht...~
+@12
 ==C#Q03009 IF ~InParty("ACBre")See("ACBRE")!StateCheck("ACBre",CD_STATE_NOTVALID)~ THEN
-~Und so dick?~
+@13
 ==ACBreJ IF ~InParty("ACBre")See("ACBRE")!StateCheck("ACBre",CD_STATE_NOTVALID)~ THEN
-~Ich... Buzzurk! Ich bin nicht dick!~
+@14
 ==C#Q03010 IF ~InParty("ACBre")See("ACBRE")!StateCheck("ACBre",CD_STATE_NOTVALID)~ THEN
-~'Zurk! *kräh* 'Zurk! 'Zurk! 'Zurk! 'Zurk!~
+@15
 ==ACBreJ IF ~InParty("ACBre")See("ACBRE")!StateCheck("ACBre",CD_STATE_NOTVALID)~ THEN
-~Hör sofort auf damit! Das ist ein böses Wort, das sagt man nicht!~
+@16
 ==C#Q03010 IF ~InParty("ACBre")See("ACBRE")!StateCheck("ACBre",CD_STATE_NOTVALID)~ THEN
-~'Zurk! 'Zurk! 'Zurk! 'Zurk! 'Zurk! 'Zurk! 'Zurk! 'Zurk! 'Zurk! 'Zurk! 'Zurk! 'Zurk! 'Zurk! 'Zurk! 'Zurk! 'Zurk!~
+@17
 ==ACBreJ IF ~InParty("ACBre")See("ACBRE")!StateCheck("ACBre",CD_STATE_NOTVALID)~ THEN
-~Bei allen Göttern, <CHARNAMEN>. Das nächste Mal suchen wir uns eine Drachenhöhle oder ein Betrachternest. Mir ist vollkommen egal, was für ein Ort das ist, so lange die Axt eines Zwergen dort noch gewürdigt wird.~
+@18
 END
 
 INTERJECT_COPY_TRANS C#Q04001 0 ACC#Q040010
 == ACBreJ IF ~InParty("ACBre")See("ACBRE")!StateCheck("ACBre",CD_STATE_NOTVALID)~ THEN
-~Verdammte Lindwurmbrut! Keine Sorge, den knöpf' ich mir vor!~
+@19
 == C#Q04001 IF ~InParty("ACBre")See("ACBRE")!StateCheck("ACBre",CD_STATE_NOTVALID)~ THEN
-~Hilfe!~
+@20
 END
 
 INTERJECT_COPY_TRANS2 C#Q04003 2 ACC#Q040032
 == ACBreJ IF ~InParty("ACBre")See("ACBRE")!StateCheck("ACBre",CD_STATE_NOTVALID)~ THEN
-~Das wird diesem Schnösel eine Lehre sein. Und wenn nicht, dann gibt es ein paar Nachhilfestunden in Sachen Benehmen auf Zwergenart.~
+@21
 END
 
